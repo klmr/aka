a535df16e73b7fa02af6a662331cdfc59dcc9649
Konrad Rudolph <konrad.rudolph@roche.com>
2024-12-31 09:23:04 +0100

    Simplify ‘testthat’ check invocation

--- tests/testthat.r
+++ tests/testthat.r
@@ -1,6 +1,1 @@
-pkg = local({
-  install_status_line = utils::tail(readLines(file.path(dirname(getwd()), '00install.out')), 1L)
-  sub('.*DONE \\((.*)\\)$', '\\1', install_status_line)
-})
-
-testthat::test_check(pkg)
+testthat::test_check(Sys.getenv('_R_CHECK_PACKAGE_NAME_'))
